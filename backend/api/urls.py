839cb6794891ef8f524076ca47c9bebbe7398f6e
Egor Remezov <mr.drode@gmail.com>
2022-11-12 23:22:27 +0300

    обновил урлы

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,24 +1,10 @@
-from django.contrib import admin
+from django.conf.urls import url
 from django.urls import include, path
 from rest_framework import routers
 
-router_v1 = routers.DefaultRouter()
-
-v1_urlpatterns = (
-    [
-        path('', include(router_v1.urls)),
-    ],
-    'v1'
-)
-
-auth_patterns = (
-    [
-        path('auth/', include('djoser.urls.authtoken')),
-    ],
-    'auth'
-)
+router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('v1/', include(v1_urlpatterns)),
-    path('v1/', include(auth_patterns)),
+    url(r'', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
