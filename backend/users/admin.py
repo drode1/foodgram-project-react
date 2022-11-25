f450080060bb9e6dc24603298a39d3439afdbd4f
Egor Remezov <mr.drode@gmail.com>
2022-11-26 01:10:46 +0300

    добавил метод в админке для вывода кол-во рецепта у юзеров

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from recipes.models import Recipe
 from users.models import User
 
 
@@ -7,5 +8,12 @@ from users.models import User
 class UserAdmin(admin.ModelAdmin):
     """ Класс для управления пользователями в админке сайта. """
 
-    list_display = ('id', 'username', 'first_name', 'last_name', 'email',)
+    list_display = ('id', 'username', 'first_name', 'last_name', 'email',
+                    'get_quantity_user_recipes',)
     list_filter = ('username', 'email',)
+
+    @admin.display(description='Кол-во рецептов')
+    def get_quantity_user_recipes(self, obj):
+        """ Подсчитывает кол-во рецептов каждого пользователя. """
+
+        return Recipe.objects.filter(author_id=obj.id).count()
