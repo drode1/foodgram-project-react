909d43caac976628f7f9a7356d0edfbaf432c52e
Egor Remezov <mr.drode@gmail.com>
2022-12-02 02:57:15 +0300

    поправил баг открытия рецепта не владельцем

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,4 +5,6 @@ class IsUserOrAdminOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     """ Права для проверки является пользователь владельцем объекта. """
 
     def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
         return request.user == obj.author
